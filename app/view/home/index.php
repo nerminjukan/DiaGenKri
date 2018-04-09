333a7581495bf1faab206684b53892e40534dfb8
Nermin <nj2245@student.uni-lj.si>
2018-04-09 22:22:32 +0200

    Created the navigation bar
    The navigation bar has been created with the bootstrap framework, still needs some minor changes in style, for now it is responsive and looks better than before. It is implemented on the 'Home', 'Log in' and 'Registration' pages.

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -1,19 +1,32 @@
 <!DOCTYPE html>
 <html lang="sl">
-<meta charset="UTF-8" content="width=device-width, initial-scale=1.0">
-<title>Title</title>
+<head>
+    <title>Home Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <header class="col-12">
-    <h1>SPLETNA BANKA </h1>
+    <h1>Home</h1>
 </header>
 
-<div class="row">
-    <nav>
-        <ul>
-            <?php if(isset($_SESSION["uporabnik"])): ?>
-            <?php else: ?>
-                <li style="display: inline"><a href="../../../DiaGenKri/public/logIn">PRIJAVA</a></li>
-                <li style="display: inline"><a href="../../../DiaGenKri/public/register">REGISTRACIJA</a></li>
-            <?php endif; ?>
-        </ul>
-
-    </nav>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">DiaGenKri</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../../../DiaGenKri/public/logIn"><span class="glyphicon glyphicon-user"></span> Log in</a></li>
+                <li><a href="../../../DiaGenKri/public/register"><span class="glyphicon glyphicon-log-in"></span> Registration</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- app/view/register/index.php
+++ app/view/register/index.php
@@ -1,20 +1,38 @@
 <!DOCTYPE html>
 <html lang="sl">
-<meta charset="UTF-8" content="width=device-width, initial-scale=1.0">
-<title>Title</title>
+<head>
+    <title>LogIn Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <header class="col-12">
-    <h1>REGISTRACIJA</h1>
+    <h1>Registration</h1>
 </header>
 
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">DiaGenKri</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../../../DiaGenKri/public/home"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+                <li><a href="../../../DiaGenKri/public/logIn"><span class="glyphicon glyphicon-user"></span> Log in</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div>
-    <nav style="text-align: center; background-color: #ecfff8; font-size: 1.7vw">
-        <ul>
-                <li style="display: inline"><a href="../../../DiaGenKri/public/home">DOMOV</a></li>
-        </ul>
-
-    </nav>
     <article>
-
         <div align = "center">
             <div style = "width:300px; border: solid 1px #333333; " align = "left">
                 <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Registracija v sistem</b></div>
