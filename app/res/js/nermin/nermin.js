ee9d2e73db134cad5958f8289b67b10eb10a2cbd
Nermin <nj2245@student.uni-lj.si>
2018-05-09 20:53:44 +0200

    Visualisation page implementation
    Created the visualisation page and controller, added separate JS files for simultaneous development, implemented test canvas and shape.

---
+++ app/res/js/nermin/nermin.js
@@ -0,0 +1,5 @@
+$(function(){
+    var paper = Raphael(document.getElementById('content'), innerWidth, innerHeight, 0, 0);
+
+    var c = paper.circle(40, 40, 20);
+});
---
+++ app/view/visualisation/index.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <title>Visualisation</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="../../../DiaGenKri/app/res/js/raphael/raphael.min.js"></script>
+    <script src="../../../DiaGenKri/app/res/js/visualisation.js"></script>
+    <link rel="stylesheet" href="../../../DiaGenKri/app/res/css/main.css"
+
+</head>
+<header class="col-12 spacing-increased">
+    <h1>Visualisation</h1>
+</header>
+
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="../../../DiaGenKri/public/home">DiaGenKri</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <?php if(isset($_SESSION["user"])): ?>
+                    <li><a href="../../../DiaGenKri/public/administrate"><span class="glyphicon glyphicon-cog"></span> Administrate</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <span class="glyphicon glyphicon-user"></span> 
+                            <strong><?php
+                                echo $_SESSION["user-name"];
+                                ?>
+                            </strong>
+                            <span class="glyphicon glyphicon-chevron-down"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <div class="navbar-login">
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <p class="text-center">
+                                                <span class="glyphicon glyphicon-user icon-size"></span>
+                                            </p>
+                                        </div>
+                                        <div class="col-lg-8">
+                                            <p class="text-left"><strong><?php
+                                                    echo $_SESSION["user-name"] . " " . $_SESSION["user-surname"];
+                                                    ?>
+                                                </strong></p>
+                                            <p class="text-left small"><?php
+                                                echo $_SESSION["user"];
+                                                ?>
+                                            </p>
+                                            <p class="text-left">
+                                                <a href="../../../DiaGenKri/public/profile" class="btn btn-primary btn-block btn-sm">My profile</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="divider"></li>
+                            <li>
+                                <div class="navbar-login navbar-login-session">
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <p>
+                                                <a href="logIn/logOutUser/" class="btn btn-danger btn-block">Log out</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                <?php else: ?>
+                    <li><a href="../../../DiaGenKri/public/logIn"><span class="glyphicon glyphicon-user"></span> Log in</a></li>
+                    <li><a href="../../../DiaGenKri/public/register"><span class="glyphicon glyphicon-log-in"></span> Registration</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid text-center">
+    <div class="row content">
+        <div class="col-sm-2 sidenav">
+            <h3>LINKS</h3>
+            <p><a href="http://www.limfom-levkemija.org/domov.html">L&L</a></p>
+        </div>
+        <div class="col-sm-8" id="content">
+
+        </div>
+        <div class="col-sm-2 sidenav">
+            <div class="well">
+                <p>ADS</p>
+            </div>
+            <div class="well">
+                <p>ADS</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="container-fluid text-center">
+    <p>©DiaGenKri</p>
+</footer>
